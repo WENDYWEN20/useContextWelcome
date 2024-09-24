782076022929efe80832382d025b8a9d64cd5919
WendyW <102200695+WENDYWEN20@users.noreply.github.com>
2024-09-24 14:03:22 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { createContext, useContext } from "react";
+
+const ThemeContext = createContext(null);
+console.log(ThemeContext)
+export default function MyApp() {
+  return (
+    <ThemeContext.Provider value="dark">
+      <Form />
+    </ThemeContext.Provider>
+  );
+}
+
+function Form() {
+  return (
+    <Panel title="Welcome">
+      <Button>Sign up</Button>
+      <Button>Log in</Button>
+    </Panel>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = "panel panel-" + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  );
+}
+
+function Button({ children }) {
+  const theme = useContext(ThemeContext);
+  const className = "button button-" + theme;
+  return <button className={className}>{children}</button>;
+}
